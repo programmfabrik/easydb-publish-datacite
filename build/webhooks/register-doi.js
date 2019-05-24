ac6ad0fcea12c2a68bdd691de8704981da39f0d8
Nils Weiher <weiher@ub.uni-heidelberg.de>
2019-05-24 12:25:26 +0200

    Refactor registerDoiForObject, update webhook structure to fit to easyDb Version 5.51

--- build/webhooks/register-doi.js
+++ build/webhooks/register-doi.js
@@ -1,4 +1,3 @@
-const ez5 = require('ez5');
 const util = require('util');
 const fs = require('fs');
 const querystring = require('querystring');
@@ -7,38 +6,29 @@ const crypto = require('crypto');
 const fetch = require('node-fetch');
 const bufferEq = require('buffer-equal-constant-time');
 
-const info = JSON.parse(process.argv[2]);
-info.paths = module.paths;
-info.env = process.env;
-const easyDbUrl = info.config.system.server.external_url;
 
 // Read configuration
 const config = require('../../config.js');
 
 
 function returnAndLogJsonError(error, status = 500) {
-  log(error);
-  console.log(JSON.stringify({
-    headers: {
-      'Content-Type': 'application/json; charset: utf-8'
-    },
-    body: JSON.stringify({error}),
-    status_code: status
-  }));
+  log(error, 'ERROR');
+
+  ez5.respondError('error.api.generic', {description: error}, status)
 }
 
 function authHeader(username, password) {
   return 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64')
 }
 
-function log(messageOrObject) {
+function log(messageOrObject, level='INFO') {
   if (typeof messageOrObject === 'object' && messageOrObject !== null) {
     messageOrObject = JSON.stringify(messageOrObject, null, 2);
   }
-  fs.appendFileSync(config.logFile, `${new Date().toISOString()} ${messageOrObject}\n`);
+  fs.appendFileSync(config.logFile, `${new Date().toISOString()} ${level} ${messageOrObject}\n`);
 }
 
-async function authenticateEasyDb(username, password) {
+async function authenticateEasyDb(username, password, easyDbUrl) {
   // get session token
   const sessionPath = '/api/v1/session';
   let response = await fetch(easyDbUrl + sessionPath);
@@ -75,16 +65,12 @@ async function authenticateEasyDb(username, password) {
 
 async function registerDoiForObject(dbObject, easyDbOpts, dataciteOpts) {
   const { username, password, endpoint: dataciteEndpoint, doiPrefix} = dataciteOpts;
-  const { xsltName, token: easyDbToken, collector } = easyDbOpts;
+  const { xsltName, token: easyDbToken, collector , url: easyDbUrl} = easyDbOpts;
   const dataciteAuth = authHeader(username, password);
-  const metadataUrl = easyDbUrl + '/api/v1/objects/uuid/' + dbObject._uuid + '/format/xslt/' + xsltName;
   const systemObjectId = dbObject._system_object_id;
   const doi = `${doiPrefix}${systemObjectId}`;
-  const objectDetailUrl = `${easyDbUrl}/detail/${systemObjectId}`;
 
-  let metadataXml = await fetch(metadataUrl).then(resp => resp.text());
-  log(`Got metadata for ${dbObject._uuid}`);
-  //TODO find better placeholder string
+  let metadataXml = await getMetadataFromEasyDb(easyDbUrl, dbObject,_uuid);
   metadataXml = metadataXml.replace('___DOI_PLACEHOLDER___', doi);
 
   const dataciteMetadataUrl = dataciteEndpoint + '/metadata/' + doi;
@@ -104,6 +90,7 @@ async function registerDoiForObject(dbObject, easyDbOpts, dataciteOpts) {
   }
   log('Success, response: ' + dataciteMetadataResponseText);
 
+  const objectDetailUrl = `${easyDbUrl}/detail/${systemObjectId}`;
   const dataciteMintUrl = `${dataciteEndpoint}/doi/${doi}`;
   const body = `doi=${doi}\nurl=${objectDetailUrl}\n`
   log(`PUT ${dataciteMintUrl} with body: ${body}`);
@@ -129,13 +116,30 @@ async function registerDoiForObject(dbObject, easyDbOpts, dataciteOpts) {
     easydb_uri: objectDetailUrl
   }
 
+  return { published: await postPublishedDoiToEasyDb(publish, easyDbUrl, easyDbToken) };
+}
+
+async function getMetadataFromEasyDb(objectUuid, easyDbUrl) {
+  // TODO Check if authentication as api-user is necessary?
+  const metadataUrl = easyDbUrl + '/api/v1/objects/uuid/' + objectUuid + '/format/xslt/' + xsltName;
+  const metadataResponse = await fetch(metadataUrl);
+  const metadataResponseBody = await metadataResponse.text();
+  if (!metadataResponse.ok) {
+    throw Error('Failed getting metadata from easyDb: '
+      + `${metadataResponse.status} ${metadataResponse.statusText}, response: ${dataciteMetadataResponseText}`);
+  }
+  log(`Got metadata for ${objectUuid}`);
+  return metadataResponseBody;
+}
+
+async function postPublishedDoiToEasyDb(publishObject, easyDbUrl, easyDbToken) {
   const publishApiUrl = easyDbUrl + '/api/v1/publish?token=' + easyDbToken;
   log(`POST ${publishApiUrl}, with publish object:`);
-  log(publish);
+  log(publishObject);
   const publishResponse = await fetch(easyDbUrl + '/api/v1/publish?token=' + easyDbToken,
   {
     method: 'post',
-    body: JSON.stringify([{publish}])
+    body: JSON.stringify([{publishObject}])
   })
   const publishResponseObject = await publishResponse.json();
   if (!publishResponse.ok ) {
@@ -145,14 +149,12 @@ async function registerDoiForObject(dbObject, easyDbOpts, dataciteOpts) {
 
   log('Success, response:');
   log(publishResponseObject);
-  return {
-    published: publishResponseObject
-  };
+  return publishResponseObject;
 }
 
-async function registerAllDOIs(objects, useConfig) {
-  const session = await authenticateEasyDb(config.easyDb.user, config.easyDb.password);
-  const easyDbOpts = Object.assign({ token: session.token }, config.easyDb);
+async function registerAllDOIs(objects, useConfig, easyDbUrl) {
+  const session = await authenticateEasyDb(config.easyDb.user, config.easyDb.password, easyDbUrl);
+  const easyDbOpts = Object.assign({ token: session.token, url: easyDbUrl}, config.easyDb);
 
   // New or updated object call async register doi and await all result
   return Promise.all(objects.map( dbObject => registerDoiForObject(dbObject, easyDbOpts, config.datacite[useConfig]) ));
@@ -168,46 +170,51 @@ function verifySignature(body, receivedSignature, secret) {
   return bufferEq(new Buffer(signature), new Buffer(receivedSignature));
 }
 
-/* How to write different responses
-   easydb Webhook Plugin expects JSON output on STDOUT.
-   We can return a JSON object with keys "headers", "body" and "status_code".
-   see https://docs.easydb.de/en/technical/plugins/webhooks/webhook/
 
-console.log(JSON.stringify({
-  headers: {
-    'Content-Type': 'text/html; charset: utf-8'
-  },
-  body: 'Info was successfully stored into file: ' + dumpfile + '.'
-}));
+/* How to write different responses
+  ez5.respondSuccess(body, statusCode = 200)
+
+  ez5.respondError(messageCode, parameters = {}, statusCode = 400)
+
+  messageCode is an localisation Key for the message.
+
+  TODO: Check how to write custom messages
+
 */
 
-// Parse query parameters
-var {useConfig = 'test'} = info.request.query_string_parameters;
-// Parse body
-if (!info.request.body) {
-  returnAndLogJsonError('Missing request body', 400);
-  return;
-}
-if (!verifySignature(info.request.body, info.request.headers['X-Hub-Signature'], config.HMACSecret)) {
-  returnAndLogJsonError('Invalid signature hash or wrong HMACSecret configured', 401);
-  return;
-}
-log(`Using config ${useConfig}`);
-try {
-  const transition = JSON.parse(info.request.body);
-  log(info.request);
-  log(transition);
-  if (!['UPDATE', 'INSERT'].includes(transition.operation)) {
-    returnAndLogJsonError('Invalid JSON body, expected "operation" key with value "UPDATE" or "INSERT" operation')
+function main(context) {
+  const easyDbUrl = context.config.system.server.external_url;
+
+  // Parse query parameters
+  var {useConfig = 'test'} = context.request.query_string_parameters;
+  // Parse body
+  if (!context.request.body) {
+    returnAndLogJsonError('Missing request body', 400);
     return;
   }
-  registerAllDOIs(transition.objects, useConfig).then( statuses => {
-    log('All registerDoiForObject finished successfully');
-    ez5.returnJsonBody({status: statuses});
-  }).catch(error => {
+  if (!verifySignature(context.request.body, context.request.headers['X-Hub-Signature'], config.HMACSecret)) {
+    returnAndLogJsonError('Invalid signature hash or wrong HMACSecret configured', 401);
+    return;
+  }
+  log(`Using config ${useConfig}`);
+  try {
+    const transition = JSON.parse(context.request.body);
+    log(context.request);
+    if (!['UPDATE', 'INSERT'].includes(transition.operation)) {
+      returnAndLogJsonError('Invalid JSON body, expected "operation" key with value "UPDATE" or "INSERT" operation')
+      return;
+    }
+    registerAllDOIs(transition.objects, useConfig, easyDbUrl).then( statuses => {
+      log('All registerDoiForObject finished successfully');
+      ez5.respondSuccess({status: statuses});
+    }).catch(error => {
+      returnAndLogJsonError(error.toString());
+    })
+  }
+  catch (error) {
     returnAndLogJsonError(error.toString());
-  })
+  }
+
 }
-catch (error) {
-  returnAndLogJsonError(error.toString());
-}
+
+module.exports = {main}
